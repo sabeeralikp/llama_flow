af66df1b431929557ead7a1df470af21a7b02661
Sabeeralikp <Sabeeralikp8@gmail.com>
2024-05-15 19:20:52 +0530

    change in colab run

--- colab_run.py
+++ colab_run.py
@@ -2,7 +2,8 @@ import os
 import nest_asyncio
 from pyngrok import ngrok
 import uvicorn
-from main import app
+
+# from main import app
 from dotenv import load_dotenv
 import multiprocessing
 
@@ -14,4 +15,4 @@ ngrok.set_auth_token(NGORK_AUTH_TOKEN)
 ngrok_tunnel = ngrok.connect(8000)
 print("Public URL:", ngrok_tunnel.public_url)
 nest_asyncio.apply()
-uvicorn.run(app, port=8000, workers=multiprocessing.cpu_count())
+uvicorn.run("main:app", port=8000, workers=multiprocessing.cpu_count())
